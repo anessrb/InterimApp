c913b638d9f9c1805c070f84f75c2ef58f8312db
anessrb <anesshiba@gmail.com>
2024-03-20 17:07:19 +0100

    page beforeintro

--- app/src/main/java/com/example/interimapplication/beforeintro.java
+++ app/src/main/java/com/example/interimapplication/beforeintro.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class intro3 extends AppCompatActivity {
+public class beforeintro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intro3);
+        setContentView(R.layout.beforeintro);
 
         Button button1 = findViewById(R.id.nextButton);
         button1.setOnClickListener(new View.OnClickListener() {
