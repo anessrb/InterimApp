71eeb63f2348df73c7c5de7c372d0f47ee858a0a
anessrb <anesshiba@gmail.com>
2024-03-21 01:51:45 +0100

    ajout de login page

---
+++ app/src/main/java/com/example/interimapplication/login.java
@@ -0,0 +1,25 @@
+package com.example.interimapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button button1 = findViewById(R.id.nextButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), login.class);
+                view.getContext().startActivity(intent);}
+        });
+    }
+}
